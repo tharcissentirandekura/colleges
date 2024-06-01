dd25bbedbb6d271f70c586608c3137be0790f3ce
tharcissentirandekura <rest.apily@gmail.com>
2024-06-01 02:12:16 +0000

    save the changes to the colleges website api

---
+++ colleges/api/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+# ORM - Object Relational Mapping
+
+class College(models.Model):
+    name = models.CharField(max_length=20)
+    location = models.CharField(max_length=20)
+    about = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.name #returning the name of the college
